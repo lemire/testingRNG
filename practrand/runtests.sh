5b5eee5da7b92724ea723a92386520d10e0126e2
Daniel Lemire <lemire@gmail.com>
2017-08-17 12:20:40 -0400

    More verbose

--- practrand/runtests.sh
+++ practrand/runtests.sh
@@ -1,9 +1,11 @@
 make -s
+MEM="2GB"
+echo "Testing "$MEM " of data per run"
 declare -a commands=('testxoroshiro128plus' 'testpcg32' 'testpcg64' 'testsplitmix64' 'testxorshift32' );
 for t in "${commands[@]}"; do
      filelog=$t.log
      echo "# RUNNING" $t  "Outputting result to " $filelog
-    ./$t | ./RNG_test stdin64 -tlmax 2GB > $filelog
+    ./$t | ./RNG_test stdin64 -tlmax $MEM > $filelog
     grep -s "FAIL" $filelog > /dev/null
     RESULT=$?
     if [ $RESULT == 1 ]; then
