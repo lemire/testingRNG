b4aa228fae78248137760931952a5b33b44cb5f8
Daniel Lemire <lemire@gmail.com>
2017-08-17 10:53:02 -0400

    Saving...

--- testu01/runtests.sh
+++ testu01/runtests.sh
@@ -1,11 +1,19 @@
 make
 declare -a commands=('testxoroshiro128plus' 'testpcg32' 'testpcg64' 'testsplitmix64' 'testxorshift32' );
-for f in  "-s" "-c" "-b" ; do
+for order in  "-r" "" ; do
+ for f in  "-s" "-c" "-b" ; do
   for t in "${commands[@]}"; do
-     filelog=$t$f.log
-     echo "# RUNNING" $t $f "Outputting result to " $filelog
+     filelog=$t$f$order.log
+     echo "# RUNNING" $t $f $order "Outputting result to " $filelog
     ./$t $f > $filelog
+    RESULT=$?
+    if [ $RESULT == 0 ]; then
+     echo "Success!"
+    else
+     echo "Failure!"
+    fi
   done
   echo
   echo
+ done
 done
