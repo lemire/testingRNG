a714daf606d63f4a13d327b7f9fe27f7d6e52a40
Daniel Lemire <lemire@gmail.com>
2017-08-17 13:39:45 -0400

    Updating script to take into account bit reversal.

--- testu01/runtests.sh
+++ testu01/runtests.sh
@@ -1,6 +1,6 @@
 make -s
 declare -a commands=('testxoroshiro128plus' 'testpcg32' 'testpcg64' 'testsplitmix64' 'testxorshift32' );
-for order in  "-r" "" ; do
+for order in  "-R" "-r" "" ; do
  for f in  "-s" "-c" "-b" ; do
   for t in "${commands[@]}"; do
      filelog=$t$f$order.log
