9f28f4a96f29da59cb9f07c37208d674d7898c0c
Daniel Lemire <lemire@gmail.com>
2017-08-23 14:12:22 -0400

    Fixed bad name

--- testu01/src/testxorshift32.c
+++ testu01/src/testxorshift32.c
@@ -4,6 +4,6 @@ static inline void thisrng_seed(uint64_t seed) { xorshift32_seed(seed); }
 
 static inline uint64_t thisrng() { return xorshift32(); }
 
-const char *name = "splitmix64";
+const char *name = "xorshift32";
 
 #include "main.h"
