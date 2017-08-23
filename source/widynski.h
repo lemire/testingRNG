634a2e9026571702695cdd0c276a9a2f50c437e3
Daniel Lemire <lemire@gmail.com>
2017-08-23 16:13:51 -0400

    Adding widynski

---
+++ source/widynski.h
@@ -0,0 +1,26 @@
+#ifndef WIDYNSKI_H
+#define WIDYNSKI_H
+
+#include <stdint.h>
+// based on https://arxiv.org/pdf/1704.00358.pdf
+static uin64_t g_widynski_x;
+static uin64_t g_widynski_w;
+static uin64_t g_widynski_s;
+
+static inline void widynski_seed(uint64_t seed) {
+    g_widynski_w = 0;
+    g_widynski_x = 0;
+    g_widynski_s = seed;
+    g_widynski_s |= 1;
+    if((g_widynski_s >> 32) == 0)
+      g_widynski_s = g_widynski_s | (g_widynski_s << 32);
+}
+
+static inline uint64_t widynski() {
+    g_widynski_x *= g_widynski_x;
+    g_widynski_x += (g_widynski_w += g_widynski_s);
+    g_widynski_x  = (g_widynski_x >> 32) | ( g_widynski_x << 32);
+    return g_widynski_x;
+}
+
+#endif
