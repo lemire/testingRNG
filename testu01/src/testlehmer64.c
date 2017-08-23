cf150fb189dd20918477e82b6fbfa61df96c9b17
Daniel Lemire <lemire@gmail.com>
2017-08-23 14:49:48 -0400

    adding lehmer64

---
+++ testu01/src/testlehmer64.c
@@ -0,0 +1,9 @@
+#include "lehmer64.h"
+
+static inline void thisrng_seed(uint64_t seed) { lehmer64_seed(seed); }
+
+static inline uint64_t thisrng() { return lehmer64(); }
+
+const char *name = "pcg64";
+
+#include "main.h"
