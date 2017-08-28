03f85927ffb013ab1761e22e2252385bfc594e70
Daniel Lemire <lemire@gmail.com>
2017-08-28 16:35:26 -0400

    Enforcing big crush

--- testu01/bigcrush.sh
+++ testu01/bigcrush.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 make -s
 
+whichcrush=" -b " #big crush
 t=$@ # this is the command
 # we use four seeds including the default seed
 declare -a seeds=('' '-S 987654 ' ' -S 848432 ' '  -S 412451 ')
@@ -11,7 +12,7 @@ for SEEDCMD in "${seeds[@]}"; do
  for order in  "-z" "-r" "" ; do
      echo "--- using bit order flag: " $order
      echo "--- using seed flag: " $SEEDCMD
-     thiscommand=$t" "$SEEDCMD" "$order
+     thiscommand=$t" "$SEEDCMD" "$order " "$whichcrush
      wf=$(echo $thiscommand | sed 's/ //g')
      filelog=$wf.log
      echo "# RUNNING" $thiscommand  "Outputting result to " $filelog
