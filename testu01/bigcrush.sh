564615acc16c0f0091c8a8cf58b4af24360e585c
Daniel Lemire <lemire@gmail.com>
2017-08-28 16:44:41 -0400

    Checking log return values.

--- testu01/bigcrush.sh
+++ testu01/bigcrush.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 make -s
-
-whichcrush=" -b " #big crush
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+wc="-b" #big crush
 t=$@ # this is the command
+
 # we use four seeds including the default seed
 declare -a seeds=('' '-S 987654 ' ' -S 848432 ' '  -S 412451 ')
 RED='\033[0;31m'
@@ -12,17 +16,23 @@ for SEEDCMD in "${seeds[@]}"; do
  for order in  "-z" "-r" "" ; do
      echo "--- using bit order flag: " $order
      echo "--- using seed flag: " $SEEDCMD
-     thiscommand=$t" "$SEEDCMD" "$order " "$whichcrush
+     thiscommand=$t" "$SEEDCMD" "$order" "$wc 
+     echo $thiscommand
      wf=$(echo $thiscommand | sed 's/ //g')
      filelog=$wf.log
      echo "# RUNNING" $thiscommand  "Outputting result to " $filelog
     ./$thiscommand  > $filelog
-    grep -s "All tests were passed" $filelog > /dev/null
-    RESULT=$?
-    if [ $RESULT == 0 ]; then
-     echo -e "${GREEN}Success!${NC}"
+    CMDRESULT=$?
+    if [ $CMDRESULT == 0 ]; then 
+      grep -s "All tests were passed" $filelog > /dev/null
+      RESULT=$?
+      if [ $RESULT == 0 ]; then
+       echo -e "${GREEN}Success!${NC}"
+      else
+       echo -e "${RED}Failure!${NC}"
+      fi
     else
-     echo -e "${RED}Failure!${NC}"
+       echo -e "${RED} command $thiscommand returned error code $CMDRESULT, log file $filelog is suspect${NC} "
     fi
   echo
   echo
