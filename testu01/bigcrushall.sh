9927626b11d7e280d0e5f598c0479db85b114ab9
Daniel Lemire <lemire@gmail.com>
2017-09-12 15:46:41 -0400

    adding to script

--- testu01/bigcrushall.sh
+++ testu01/bigcrushall.sh
@@ -3,7 +3,7 @@
 make -s
 parent=$(dirname "$0")
 
-declare -a commands=('testsplitmix64 -H' 'testsplitmix64' 'testpcg32' 'testpcg64 -H' 'testpcg64' 'testxoroshiro128plus -H' 'testxoroshiro128plus' 'testxorshift128plus -H' 'testxorshift128plus' 'testlehmer64' 'testlehmer64 -H' 'testaesctr'  'testxorshift32' 'testmersennetwister' );
+declare -a commands=('testsplitmix64 -H' 'testsplitmix64' 'testpcg32' 'testpcg64 -H' 'testpcg64' 'testxoroshiro128plus -H' 'testxoroshiro128plus' 'testxorshift128plus -H' 'testxorshift128plus' 'testlehmer64' 'testlehmer64 -H' 'testaesctr'  'testxorshift32' 'testmersennetwister' 'testxorshift1024star' 'testxorshift1024star -H' );
 for t in "${commands[@]}"; do
    $parent/bigcrush.sh $t 
 done # for t in "${commands[@]}"; do
