10482e1f9c2085a93e5a8d2f61374f9d28cd6ab8
Daniel Lemire <lemire@gmail.com>
2017-09-12 15:10:10 -0400

    Adding mersenne twister to the lot

--- testu01/bigcrushall.sh
+++ testu01/bigcrushall.sh
@@ -3,7 +3,7 @@
 make -s
 parent=$(dirname "$0")
 
-declare -a commands=('testsplitmix64 -H' 'testsplitmix64' 'testpcg32' 'testpcg64 -H' 'testpcg64' 'testxoroshiro128plus -H' 'testxoroshiro128plus' 'testxorshift128plus -H' 'testxorshift128plus' 'testlehmer64' 'testlehmer64 -H' 'testaesctr'  'testxorshift32' );
+declare -a commands=('testsplitmix64 -H' 'testsplitmix64' 'testpcg32' 'testpcg64 -H' 'testpcg64' 'testxoroshiro128plus -H' 'testxoroshiro128plus' 'testxorshift128plus -H' 'testxorshift128plus' 'testlehmer64' 'testlehmer64 -H' 'testaesctr'  'testxorshift32' 'testmersennetwister' );
 for t in "${commands[@]}"; do
    $parent/bigcrush.sh $t 
 done # for t in "${commands[@]}"; do
