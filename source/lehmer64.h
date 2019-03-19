60b5a9c1349e1346ea97ad79c7bc210f57c7bdd1
Daniel Lemire <lemire@gmail.com>
2019-03-19 11:17:11 -0400

    Removing typo.

--- source/lehmer64.h
+++ source/lehmer64.h
@@ -24,7 +24,6 @@ static inline void lehmer64_seed(uint64_t seed) {
 
 static inline uint64_t lehmer64() {
   g_lehmer64_state *= UINT64_C(0xda942042e4dd58b5);
-  ;
   return g_lehmer64_state >> 64;
 }
 
