0e131bc4904879130826479b3455053abf5d6466
Daniel Lemire <lemire@gmail.com>
2022-09-16 16:13:34 -0400

    Update testwidynski.c

--- practrand/src/testwidynski.c
+++ practrand/src/testwidynski.c
@@ -7,12 +7,6 @@
 
 #define buffer_size 512
 int main(int argc, char **argv) {
-  int c;
-  while ((c = getopt(argc, argv, "")) != -1)
-    switch (c) {
-    default:
-      abort();
-    }
   uint64_t seedvalue = 12345678;
   widynski_seed(seedvalue);
   uint32_t buffer[buffer_size];
