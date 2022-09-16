f5db50bf6a2d5113080bbb4d726cbca3f75aa625
Daniel Lemire <lemire@gmail.com>
2022-09-16 16:12:52 -0400

    Update testwyhash.c

--- practrand/src/testwyhash.c
+++ practrand/src/testwyhash.c
@@ -7,12 +7,6 @@
 
 #define buffer_size 512
 int main(int argc, char **argv) {
-  int c;
-  while ((c = getopt(argc, argv, "")) != -1)
-    switch (c) {
-    default:
-      abort();
-    }
   uint64_t seedvalue = 12345678;
   wyhash64_seed(seedvalue);
   uint32_t buffer[buffer_size];
