c4a7b4740259dc285e591b892249e1adbdfe886b
TomaszRD <135917491+TomaszRD@users.noreply.github.com>
2023-06-22 03:51:47 +0200

    Update CG64.h

--- source/CG64.h
+++ source/CG64.h
@@ -4,6 +4,36 @@
 #include "splitmix64.h"
 #include <stdint.h>
 
+/* Written in 2023 by Tomasz R. Dziala (tomasz-dziala@wp.pl).
+ 
+This is 64-bit Collatz Generator: CG-64. All purpose pseudo random generator,
+with excellent statistical quality (it passes all test we aware of) and superb speed.
+ 
+The state may be seeded with any numbers, except c[0], which has to be odd, but we suggest
+to seed c[0] by 64-bit odd number and set all other elements equal zero, for simplicity.
+It is suitable for large-scale parallel computations, since each generator seeded by unike odd c[0]
+produce separate stream of period at least 2^64. 
+ 
+For full independence of 2^64 streams, the first 48 outputs of each stream should be skipped, 
+otherwise minor correlations may occur in the first outputs on the low bits of successively 
+initialized streams, especially if one initalize streams by numbers 1, 3, 5, ... or other counter. 
+For this purpose user may call initalizator(). Using initalizator() every stream may be initialized 
+with any 64-bit odd number. Skipping the first few outputs is also required to fill all the state
+of the generator by bits, however this can be also done by proper choose of the seed. 
+ 
+For faster initialization user may seed a Splitmix64 generator and use its outputs to serially fill c[0]. 
+
+This is initalizator for the Collatz Generator. It is equivalent to 48 calls to next();
+it could be used for initializing independent streams for parallel computations.
+ 
+void initializator(void)
+{
+    for (int i = 0; i < 48; i++)
+    {
+        c[1] = (c[1] >> 1) * ((c[2] += c[1]) | 1) ^ (c[3] += c[0]);
+    }
+} */
+
 static uint64_t CG64_c[4];
 
 // call this one before calling CG64
